fc250ec8562fa16ec956302f599088a47fbaf2bf
yyuuii <yppseabattle@gmail.com>
2018-04-26 15:19:50 -0400

    Added constants file with cannonball damage multipliers and ship list

---
+++ src/resources/constants.js
@@ -0,0 +1,21 @@
+export const cbDmg = {
+    small: 1,
+    medium: 1.5,
+    large: 2
+};
+
+export const ships = [
+    'Sloop',
+    'Cutter',
+    'Dhow',
+    'Fanchuan',
+    'Longship',
+    'Baghlah',
+    'Merchant Brig',
+    'Junk',
+    'War Brig',
+    'Merchant Galleon',
+    'Xebec',
+    'War Frigate',
+    'Grand Frigate'
+];
