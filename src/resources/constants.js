98183b81a5f109a41858de6a70aae1887595bf89
yyuuii <yppseabattle@gmail.com>
2018-04-26 15:34:13 -0400

    indent using 2 spaces

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,21 +1,21 @@
 export const cbDmg = {
-    small: 1,
-    medium: 1.5,
-    large: 2
+  small: 1,
+  medium: 1.5,
+  large: 2
 };
 
 export const ships = [
-    'Sloop',
-    'Cutter',
-    'Dhow',
-    'Fanchuan',
-    'Longship',
-    'Baghlah',
-    'Merchant Brig',
-    'Junk',
-    'War Brig',
-    'Merchant Galleon',
-    'Xebec',
-    'War Frigate',
-    'Grand Frigate'
+  'Sloop',
+  'Cutter',
+  'Dhow',
+  'Fanchuan',
+  'Longship',
+  'Baghlah',
+  'Merchant Brig',
+  'Junk',
+  'War Brig',
+  'Merchant Galleon',
+  'Xebec',
+  'War Frigate',
+  'Grand Frigate'
 ];
