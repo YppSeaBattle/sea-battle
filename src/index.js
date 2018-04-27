8a2adeb417a384177e14b258e9e69e687e9dbcb4
yyuuii <yppseabattle@gmail.com>
2018-04-27 12:33:08 -0400

    Added webfontloader and pirate font

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Pirata One', 'Material Icons'],
+  },
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
