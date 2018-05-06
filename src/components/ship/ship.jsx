517179e96d98102874384fbc6c3bab132497b21e
yyuuii <yppseabattle@gmail.com>
2018-05-06 19:43:07 -0400

    Pass ship state through props

--- src/components/ship/ship.jsx
+++ src/components/ship/ship.jsx
@@ -2,23 +2,12 @@ import React, { Component } from 'react';
 import './ship.css';
 
 class Ship extends Component {
-  constructor(props) {
-    super(props);
-    this.config = require(`../../resources/ships/${this.props.shipType}.json`);
-    this.state = {
-      name: this.config.name,
-      currentDmg: 0,
-      cbSize: this.config.cbSize,
-      sizeClass: this.config.sizeClass,
-      ramDmg: this.config.ramDmg,
-      maxSFDmg: this.config.maxSFDmg,
-      maxDmg: this.config.maxDmg
-    }
-  } 
-
   render() {
     return(
-      <div className="ship">I'm a {this.state.name}</div>
+      <div className="ship">
+        <div>I'm a {this.props.name}</div>
+        <div>Dmg: {this.props.currentDmg}/{this.props.maxSFDmg}</div>
+      </div>
     );
   }
 }
