98cf48c92c5ee2ca1db907400d8c9254df38cc95
yyuuii <yppseabattle@gmail.com>
2018-05-17 17:16:25 -0400

    Removed unnecessary string

--- src/components/ship/ship.jsx
+++ src/components/ship/ship.jsx
@@ -6,7 +6,6 @@ class Ship extends Component {
   render() {
     return(
       <div className="ship">
-        <div>I'm a {this.props.name}</div>
         <div>Dmg: {this.scaleDmg(this.props.currentDmg)}/{this.scaleDmg(this.props.maxSFDmg)}</div>
       </div>
     );
